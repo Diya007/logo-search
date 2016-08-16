f733908032dccf7883dcc0c24f6e6398381f204c
Diya Zhao <zdy.gogo@msn.com>
2016-08-16 16:03:58 -0700

    add js function

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-$(function(){
-$(".button").on("click",function(){
-	$(".logos").addClass(".aci acm afe")
-})
+
+	
+	$("#search").on("click",function(){
+	
+		$(".aci, .acm, .afe").addClass("search-all")
+
+		var program = $("#programType option:selected").text()
+		var education =$("#level-of-education option:selected").text()
+
+		if (program === "SEARCH ALL" && education===
+			"SEARCH ALL") {
+			$(".aci, .acm, .afe").removeClass("search-all")
+
+		}
+
+		else if(program !== "SEARCH ALL" && education!=="SEARCH ALL" ) {
+			$(".aci."+program+"."+education).removeClass("search-all");
+			$(".acm."+program+"."+education).removeClass("search-all");
+			$(".afe."+program+"."+education).removeClass("search-all");
+		}
+
+
+	})
 
 
 
@@ -9,8 +28,3 @@ $(".button").on("click",function(){
 
 
 
-
-
-
-
-})
